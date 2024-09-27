a8a9d4de03c13e350d40db87d68659d6577eb45c
Andrea Palladino <andreapalladino@MacBook-Pro-di-Andrea.local>
2024-09-27 14:18:07 +0200

    update unit test

--- tests/test_simulations.py
+++ tests/test_simulations.py
@@ -1,14 +1,15 @@
+import pandas as pd
+import numpy as np
 from src.data import import_data
 from src.simulations import simulate_single_investment
 from math import isclose
 
 def test_single_investment():
 
-    df = import_data('^SP500TR', starting_date='2000-01-01')
-    df = df.loc[df.index <='2003-12-31']
-    
-    assert len(df) == 1004, "Check the length of the DataFrame"
+    interest = 5
+    tax = 26
+    df = pd.DataFrame(np.linspace(100, 100+interest*4, 1008), columns=['close'])    
     
     final_return = simulate_single_investment(df, n_years=2, starting_point=5, verbose=False)
-    
-    assert isclose(final_return, -11.24, abs_tol=10**-2)
+
+    isclose(final_return, interest*(1-tax/100), abs_tol=10**-1)
